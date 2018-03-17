3b891d3d47517a33770f0a75e56d85edb7215bdd
patrickun <37467986+patrickun@users.noreply.github.com>
2018-03-17 18:50:45 +0700

    Update installer.sh

--- .gitignore/installer.sh
+++ .gitignore/installer.sh
@@ -4,3 +4,5 @@ sudo apt-get install mkusb mkusb-nox usb-pack-efi
 sudo apt-get install --reinstall mkusb guidus dus mkusb-common
 sudo wget http://phillw.net/isos/linux-tools/uefi-n-bios/dd_text_16.04-UEFI-n-BIOS_2017-05-07_intel-4-pendrive-7.8GB.img.xz
 sudo mkusb-nox dd_text_16.04-UEFI-n-BIOS_2017-05-07_intel-4-pendrive-7.8GB.img.xz
+sudo -
+mount /dev/sdd1 /mnt/root/boot/efi
