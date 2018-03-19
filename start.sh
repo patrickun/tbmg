a9f256a3939cc652a09ecce7f00a817e5de59232
patrickun <37467986+patrickun@users.noreply.github.com>
2018-03-19 19:07:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ export GPU_MAX_HEAP_SIZE=100
 export GPU_USE_SYNC_OBJECTS=1
 export GPU_SINGLE_ALLOC_PERCENT=100
 
-/bin/ethminer -G --farm-recheck 200 -S asia1.ethermine.org -O 0xD93f38fd49b159c223458c4ACb8A228a9Bd80Bf2.TBMG1 --cl-local-work 256 --cl-kernel 1 --cl-parallel-hash 2
+/bin/ethminer -G --farm-recheck 200 -S asia1.ethermine.org:14444 -O 0xD93f38fd49b159c223458c4ACb8A228a9Bd80Bf2.TBMG1 --cl-local-work 256 --cl-kernel 1 --cl-parallel-hash 2
