3b4ffa5c90840f83959ddcbf4a1ee9ed5ab10855
patrickun <37467986+patrickun@users.noreply.github.com>
2018-03-19 18:04:36 +0700

    Update installer.sh

--- .gitignore/installer.sh
+++ .gitignore/installer.sh
@@ -1,9 +1,5 @@
 #NonLinux Users Please burn xubuntu-1604
-sudo add-apt-repository ppa:mkusb/ppa 
-sudo apt-get update
-sudo apt-get install mkusb mkusb-nox usb-pack-efi
-sudo apt-get install --reinstall mkusb guidus dus mkusb-common
-sudo wget http://phillw.net/isos/linux-tools/uefi-n-bios/dd_text_16.04-UEFI-n-BIOS_2017-05-07_intel-4-pendrive-7.8GB.img.xz
+
 sudo mkusb-nox dd_text_16.04-UEFI-n-BIOS_2017-05-07_intel-4-pendrive-7.8GB.img.xz
 sudo -
 mount /dev/sdd1 /mnt/root/boot/efi
